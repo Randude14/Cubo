85e71182910978aaff5a9146cd0717a6c4a271ee
Randude14 <Rander510@aol.com>
2022-03-09 22:02:20 -0600

    Add basic cube class and pawn

---
+++ Source/Cubo/CuboBlock.cpp
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CuboBlock.h"
+
+ACuboBlock::ACuboBlock()
+{
+
+}
+
+// Called when the game starts or when spawned
+void ACuboBlock::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ACuboBlock::SetColor(const FLinearColor& CubeColor)
+{
+	
+}
---
+++ Source/Cubo/CuboBlock.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/StaticMeshActor.h"
+#include "CuboBlock.generated.h"
+
+UCLASS()
+class CUBO_API ACuboBlock : public AStaticMeshActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ACuboBlock();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	UFUNCTION(BlueprintCallable)
+	void SetColor(const FLinearColor& CubeColor);
+};
